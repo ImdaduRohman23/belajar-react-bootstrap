d475f6400f7c80ec43c490b421168358ab7b52b4
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-08-21 13:30:31 +0700

    coba alert succes

--- src/components/LearnAlert.jsx
+++ src/components/LearnAlert.jsx
@@ -31,6 +31,20 @@ const LearnAlert = () => {
             Memang gak mudah. Plizzz, jangan nyerah!
         </Alert>
 
+        <Alert variant="success">
+            <Alert.Heading>Hey, nice to see you</Alert.Heading>
+            <p>
+                Aww yeah, you successfully read this important alert message. This
+                example text is going to run a bit longer so that you can see how
+                spacing within an alert works with this kind of content.
+            </p>
+            <hr />
+            <p className="mb-0">
+                Whenever you need to, be sure to use margin utilities to keep things
+                nice and tidy.
+            </p>
+            </Alert>
+
         <>
             <Alert show={show} variant="success">
                 <Alert.Heading>How's it going?!</Alert.Heading>
