550c9a0c1abade95bd0ababcbf335ef357dac22c
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-08-21 12:50:34 +0700

    belajar alert

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import './App.css'
 import GridSystem from './components/GridSystem'
 import Forms from './components/Forms'
 import StackComp from './components/StackComp'
+import LearnAlert from './components/LearnAlert'
 
 function App() {
 
   return (
     <div className="App">
       {/* <Forms /> */}
-      <StackComp />
+      {/* <StackComp /> */}
+      <LearnAlert />
     </div>
   )
 }
