4a171f05c5d17aa2e7a1c110de3fcb9b71b121c6
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-08-21 13:24:59 +0700

    mengubah alert

--- src/components/LearnAlert.jsx
+++ src/components/LearnAlert.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Alert } from 'react-bootstrap'
+import { Alert, Button } from 'react-bootstrap'
+import { useState } from 'react';
 
 const LearnAlert = () => {
+    const [show, setShow] = useState(true);
+
+
     return (
     <div>
-        {[
+        {/* {[
             'primary',
             'secondary',
             'success',
@@ -17,7 +21,34 @@ const LearnAlert = () => {
             <Alert key={variant} variant={variant}>
             This is a {variant} alert—check it out!
             </Alert>
-        ))}
+        ))} */}
+
+        <Alert variant='info'>
+            Lagi belajaran bikin Alert!
+        </Alert>
+
+        <Alert variant='danger'>
+            Memang gak mudah. Plizzz, jangan nyerah!
+        </Alert>
+
+        <>
+            <Alert show={show} variant="success">
+                <Alert.Heading>How's it going?!</Alert.Heading>
+                <p>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
+                lacinia odio sem nec elit. Cras mattis consectetur purus sit amet
+                fermentum.
+                </p>
+                <hr />
+                <div className="d-flex justify-content-end">
+                <Button onClick={() => setShow(false)} variant="outline-success">
+                    Close me y'all!
+                </Button>
+                </div>
+            </Alert>
+
+            {!show && <Button onClick={() => setShow(true)}>Show Alert</Button>}
+        </>
     </div>
     )
 }
